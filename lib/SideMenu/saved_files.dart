078da36a7952a6b0b9f8ce415e230e2235e85991
aanyamittal <63955873+aanyamittal@users.noreply.github.com>
2021-03-29 00:02:16 +0530

    Update saved_files.dart

--- lib/SideMenu/saved_files.dart
+++ lib/SideMenu/saved_files.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:open_file/open_file.dart';
 
 class SavedFilesPage extends StatefulWidget {
   @override
@@ -6,8 +9,39 @@ class SavedFilesPage extends StatefulWidget {
 }
 
 class _SavedFilesPageState extends State<SavedFilesPage> {
+  Future<String> getFilePath() async {
+    Directory appDocumentsDirectory = await getApplicationDocumentsDirectory();
+    String appDocumentsPath = appDocumentsDirectory.path;
+    String filePath = '$appDocumentsPath/notes.txt';
+
+    return filePath;
+  }
+
+  void readFile() async {
+    File file = File(await getFilePath());
+    String fileContent = await file.readAsString();
+    print(fileContent);
+  }
+
+  void openFile() async {
+    OpenFile.open("${await getFilePath()}");
+  }
+
   @override
   Widget build(BuildContext context) {
-    return new Scaffold(appBar: new AppBar(title: new Text('Saved Files')));
+    return new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Saved Files'),
+        ),
+        body: ListView(
+          children: [
+            GestureDetector(
+              child: Text("notes.txt"),
+              onTap: () {
+                openFile();
+              },
+            )
+          ],
+        ));
   }
 }
